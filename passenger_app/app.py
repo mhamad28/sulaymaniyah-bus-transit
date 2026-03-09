54aed8f3fbd5d2246e5ce4b06e2b1f1828c05a62
mhamad28 <mhamadamir39@gmail.com>
2026-03-09 03:39:01 +0100

    Update app.py

--- passenger_app/app.py
+++ passenger_app/app.py
@@ -1,1 +1,8 @@
+import streamlit as st
 
+st.set_page_config(layout="wide")
+
+st.title("🚍 Sulaymaniyah Bus Transit")
+st.subheader("Passenger Portal")
+
+st.write("Passenger navigation system coming online.")
