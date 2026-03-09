d88f20a51edb43f4ccee7fa89d167cf2253bf220
mhamad28 <mhamadamir39@gmail.com>
2026-03-09 03:40:16 +0100

    Create db.py

---
+++ shared/db.py
@@ -0,0 +1,7 @@
+import streamlit as st
+from supabase import create_client
+
+def get_supabase():
+    url = st.secrets["SUPABASE_URL"]
+    key = st.secrets["SUPABASE_KEY"]
+    return create_client(url, key)
